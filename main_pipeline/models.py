b8e68bcebb1b9dae2698f24fcd8c517f062122a4
Giovanni Gatti Pinheiro <gatti@eurecom.fr>
2025-04-10 16:21:12 +0200

    factuality ranking

--- main_pipeline/models.py
+++ main_pipeline/models.py
@@ -11,6 +11,7 @@ class Sample(pydantic.BaseModel):
     thinking: Dict[str, str] = {}
     blacklisted: Optional[List[str]] = None
     factual_data: Optional[List[str]] = None
+    ranked_factual_data: Optional[List[str]] = None
     answers: Dict[str, str] = {}
 
     def is_initialized(self) -> bool:
@@ -23,16 +24,24 @@ class Sample(pydantic.BaseModel):
                 len(self.raw_factual_data) > 0
         )
 
+    def is_ranked(self) -> bool:
+        return (
+                self.is_initialized() and
+                self.blacklisted is not None and
+                self.ranked_factual_data is not None and
+                len(self.ranked_factual_data) == len(self.raw_factual_data)
+        )
+
+    def is_filtered(self) -> bool:
+        return (
+                self.is_ranked() and
+                self.factual_data is not None and
+                len(self.factual_data) <= len(self.ranked_factual_data)
+        )
+
     def is_valid(self) -> bool:
         return (
-                self.question is not None and
-                self.ground_truth is not None and
-                self.with_brackets and
-                self.raw_factual_data is not None and
-                len(self.raw_factual_data) > 0 and
-                self.blacklisted is not None and
-                self.factual_data is not None and
-                len(self.factual_data) > 0 and
+                self.is_ranked() and
                 self.answers.keys() == {"A0", "A1", "A2", "A3", "A4"} and
                 self.thinking.keys() == {"A1", "A2", "A3", "A4"}
         )
@@ -41,6 +50,9 @@ class Sample(pydantic.BaseModel):
 class Tracker(pydantic.BaseModel):
     input_samples: int = 0
     find_factual_data_error: int = 0
+    json_parse_ranking_error: int = 0
+    index_ranking_error: int = 0
+    ranking_factual_data_error: int = 0
     output_samples: int = 0
 
 
--- main_pipeline/noise_pipeline.py
+++ main_pipeline/noise_pipeline.py
@@ -2,9 +2,11 @@
 import abc
 import argparse
 import json
+import math
 import pathlib
 import random
 import re
+from json import JSONDecodeError
 from typing import List, Dict, Tuple, Set, Union, Iterator
 
 import spacy
@@ -223,7 +225,7 @@ class FactualDataStep(Step):
         sample.raw_factual_data = matches
 
 
-class FilterItemsFromQuestionStep(Step):
+class BlacklistItemsFromQuestionStep(Step):
     """
     Filters out any factual item that has overlapping words with the question.
     If any token in the bracketed item appears in the question, we skip that item.
@@ -249,7 +251,85 @@ class FilterItemsFromQuestionStep(Step):
             term.lower() for term in sample.raw_factual_data
             if any(word.lower() in question_words for word in term.split())
         ]
-        sample.factual_data = [i for i in sample.raw_factual_data if i.lower() not in sample.blacklisted]
+
+
+class RankFactualDataStep(Step):
+    """
+    Ranks factual items by order of importance.
+    """
+
+    PROMPT = (
+        "Output the indexes of terms in square brackets [ ] from the text between triple backticks ``` "
+        "by terms that shape what the text is about, who it involves, and consequences. "
+        "Downrank marked terms that are vague references, general connectors, or dependent on other terms in square "
+        "brackets. You are given a free space to decide your ranking strategy between the tags <thinking></thinking>\n"
+        "\n"
+        "Example:\n"
+        "\n"
+        "```\n"
+        "Recent studies have shown [a correlation:0] between [social media use:1] and [increased anxiety:2] among "
+        "[teenagers:3]. Although some researchers argue that online interaction can promote [social connection:4], "
+        "others warn about [its impact:5] on [self-esteem:6] and [sleep patterns:7]. Debates intensified after a "
+        "[whistleblower:8] revealed internal data from [a major tech company:9] indicating [awareness:10] of "
+        "[these risks:11].\n"
+        "```\n"
+        "<thinking>\n"
+        "The main terms are [social media use:1], [teenagers:3], [a major tech company:9]..."
+        "Did I forget any missing terms?"
+        "..."
+        "</thinking>\n"
+        "OUTPUT: [1, 3, 9, 2, 8, 6, 7, 4, 10, 0, 11, 5]"
+    )
+
+    def __init__(self, llm: LLM, max_retries: int = 8):
+        self._llm = llm
+        self._max_retries = max_retries
+
+    def step(self, sample: Sample, tracker: Tracker) -> None:
+        if not sample.is_initialized():
+            return
+
+        text = sample.with_brackets["A0"]
+
+        terms = re.findall(r'\[([^]]+)]', text)
+        for idx, term in enumerate(terms):
+            text = text.replace(f'[{term}]', f'[{term}:{idx}]', 1)
+
+        prompt = f"{RankFactualDataStep.PROMPT}\n\nNow it's your turn.\n\n```\n{text}\n```\nOUTPUT: "
+
+        for _ in range(self._max_retries):
+            llm_judgement = self._llm.query(messages=[{"role": "user", "content": prompt}])
+            _, ranks_str = llm_judgement.split("OUTPUT: ")
+
+            try:
+                ranks = json.loads(ranks_str.strip())
+            except JSONDecodeError:
+                tracker.json_parse_ranking_error += 1
+                continue
+
+            if sorted(ranks) == list(range(len(sample.raw_factual_data))):
+                terms = [sample.raw_factual_data[i] for i in ranks]
+                sample.ranked_factual_data = terms
+                return
+
+            tracker.index_ranking_error += 1
+
+        tracker.ranking_factual_data_error += 1
+
+
+class FilterFactualDataStep(Step):
+
+    def __init__(self, keep: float = 0.8):
+        if not 0. < keep <= 1.:
+            raise ValueError(f"Should be a percentage of items to keep, but got {keep}")
+
+        self._keep = keep
+
+    def step(self, sample: Sample, tracker: Tracker) -> None:
+        if not sample.is_ranked():
+            return
+
+        sample.factual_data = sample.ranked_factual_data[:math.ceil(len(sample.ranked_factual_data) * self._keep)]
 
 
 class CreateNoiseExamplesStep(Step):
@@ -260,15 +340,27 @@ class CreateNoiseExamplesStep(Step):
         self._levels = levels
 
     @classmethod
-    def split_groups(cls, idx: List[int], num: int) -> List[List[int]]:
-        group_size = len(idx) // num
-        remainder = len(idx) % num
-        groups = []
-        start = 0
-        for i in range(num):
-            current_size = group_size + 1 if i < remainder else group_size
-            groups.append(idx[start:start + current_size])
-            start += current_size
+    def batch(cls, iterable, n=1):
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx:min(ndx + n, l)]
+
+    @classmethod
+    def split_groups(cls, num_terms: int, num_groups: int) -> List[List[int]]:
+        batches = list(CreateNoiseExamplesStep.batch(list(sorted(range(num_terms), reverse=True)), num_groups))
+        groups = [[None, ] * len(batches) for _ in range(num_groups)]
+        for j, idx in enumerate(batches):
+            if j % 2 == 0:
+                for i in range(num_groups):
+                    groups[i][j] = idx[i] if i < len(idx) else None
+            else:
+                for _i, i in enumerate(reversed(range(num_groups))):
+                    groups[i][j] = idx[_i] if _i < len(idx) else None
+
+        for group in groups:
+            while None in group:
+                group.remove(None)
+
         return groups
 
     @classmethod
@@ -282,12 +374,11 @@ class CreateNoiseExamplesStep(Step):
         )
 
     def step(self, sample: Sample, tracker: Tracker) -> None:
-        if not sample.is_initialized():
+        if not sample.is_ranked():
             return
 
-        idx = list(range(len(sample.factual_data)))
-        random.shuffle(idx)
-        groups = CreateNoiseExamplesStep.split_groups(idx, self._levels)
+        groups = CreateNoiseExamplesStep.split_groups(len(sample.factual_data), self._levels)
+        random.shuffle(groups)
         a0 = sample.with_brackets["A0"]
         noised_sample = a0
         for i, group in enumerate(groups, start=1):
@@ -334,7 +425,9 @@ if __name__ == "__main__":
         Pipeline()
         .with_step(ParaphraseStep(llm))
         .with_step(FactualDataStep(nlp))
-        .with_step(FilterItemsFromQuestionStep())
+        .with_step(BlacklistItemsFromQuestionStep())
+        .with_step(RankFactualDataStep(llm))
+        .with_step(FilterFactualDataStep())
         .with_step(CreateNoiseExamplesStep(llm))
     )
 
